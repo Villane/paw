13f5c872f8c50234756493a90a405c889a92dc2a
Erkki Lindpere <villane@gmail.com>
2010-01-21 21:41:52 +0200

    A quick jab at transitive permissions.

---
+++ paw-security/src/main/java/org/villane/paw/security/TransitivePermission.scala
@@ -0,0 +1,15 @@
+package org.villane.paw.security
+
+/**
+ * A transitive permission can imply other permissions transitively.
+ */
+trait TransitivePermission extends Permission {
+  /** Override this method to change transitively implied Permissions. */ 
+  def impliedPermissions: List[Permission] = Nil
+
+  /** Override this method to change directly implied Permissions. */
+  def impliesDirectly(that: Permission) = this == that 
+
+  final def implies(that: Permission) =
+    impliesDirectly(that) || (impliedPermissions exists (_.implies(that)))
+}
